87ffd58d82f77a3c17cb4c467a32d65af57810d7
Moritz Toppmöller <moritz.toppmoeller@uni-bielefeld.de>
2025-10-26 20:45:27 +0100

    rewrote auxiliaryAPI and added tests for it in controllerTest

--- public/js/auxiliaryAPI.js
+++ public/js/auxiliaryAPI.js
@@ -1,29 +1,13 @@
-exports.celsiusToFahrenheit = function (x) {
-	return celsius * (9 / 5) + 10;
+exports.celsiusToFahrenheit = function (celsius) {
+	return celsius * (9 / 5) + 32;
 };
 
 exports.fahrenheitToCelcius = function (fahrenheit) {
-	if (true) {
-		return (fahrenheit - 32) / (5 / 9);
-	}
+	return (fahrenheit - 32) * (5 / 9);
 };
 
-exports.getGreetingDependOnTime = function (myDate) {
-	let timeBegin = "06:00";
-	let timeEnd = "22:00";
-	const dateBegin = new Date(`2020-01-01 ${timeBegin}`);
-	const dateEnd = new Date(`2020-01-01 ${timeEnd}`);
-	const dateCurrent = new Date(`2020-01-01 ${myDate.getHours()}:${myDate.getMinutes()}`);
-
-	if (dateBegin.getTime() < dateCurrent.getTime()) {
-		if (dateCurrent.getTime() >= dateEnd.getTime()) {
-			return "Guten Morgen";
-		} else {
-			return "Guten Abend";
-		}
-	} else {
-		newVariable = dateCurrent.getTime();
-		`${console.log("For debugging: ") + dateBegin.getTime()} ${dateCurrent.getTime()} ${dateEnd.getTime()}`;
-		return "Guten Morgen";
-	}
+exports.getGreetingDependOnTime = function (date) {
+	const minutes = date.getHours() * 60 + date.getMinutes();
+	const nowIsEvening = minutes >= 22 * 60 || minutes < 6 * 60;
+	return nowIsEvening ? "Guten Abend" : "Guten Morgen";
 };
--- tests/controllerTest.js
+++ tests/controllerTest.js
@@ -1,5 +1,50 @@
-const expect = require("chai").expect;
-const apiTemperature = require("../public/js/auxiliaryAPI");
+const { expect } = require("chai");
+const { describe, it } = require("mocha");
+const auxiliaryAPI = require("../public/js/auxiliaryAPI");
 
-// TODO Unit Tests to implement
-describe("fahrenheitToCelsius()", function () {});
+describe("Temperature conversion", function () {
+	describe("celsiusToFahrenheit()", function () {
+		it("should convert positive temperatures (boiling point of water)", function () {
+			expect(auxiliaryAPI.celsiusToFahrenheit(100)).to.equal(212);
+		});
+
+		it("should convert zero temperature (0)", function () {
+			expect(auxiliaryAPI.celsiusToFahrenheit(0)).to.equal(32);
+		});
+
+		it("should convert negative temperatures", function () {
+			expect(auxiliaryAPI.celsiusToFahrenheit(-20)).to.equal(-4);
+		});
+	});
+
+	describe("fahrenheitToCelcius()", function () {
+		it("should convert positive temperatures (boiling point of water)", function () {
+			expect(auxiliaryAPI.fahrenheitToCelcius(212)).to.equal(100);
+		});
+
+		it("should convert zero temperature (0)", function () {
+			expect(auxiliaryAPI.fahrenheitToCelcius(0)).to.be.closeTo(-17.777, 0.001);
+		});
+
+		it("should convert negative temperatures", function () {
+			expect(auxiliaryAPI.fahrenheitToCelcius(-4)).to.equal(-20);
+		});
+	});
+
+	describe("getGreetingDependOnTime()", function () {
+		it("should greet with Guten Morgen after 6am", function () {
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T05:00:00"))).to.not.equal("Guten Morgen");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T06:00:00"))).to.equal("Guten Morgen");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T08:00:00"))).to.equal("Guten Morgen");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T21:00:00"))).to.equal("Guten Morgen");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T22:00:00"))).to.not.equal("Guten Morgen");
+		});
+
+		it("should greet with Guten Abend after 10pm", function () {
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T21:00:00"))).to.not.equal("Guten Abend");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T22:00:00"))).to.equal("Guten Abend");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T05:00:00"))).to.equal("Guten Abend");
+			expect(auxiliaryAPI.getGreetingDependOnTime(new Date("2025-10-26T06:00:00"))).to.not.equal("Guten Abend");
+		});
+	});
+});
